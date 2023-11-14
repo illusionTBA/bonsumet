e59cc66ecfdffbf9794d16d040464a24df8ff714
illusionTBA <illusionztba@gmail.com>
2023-11-14 16:22:24 -0500

    build scripts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,9 @@
-
 import { JsonResponse } from "../utils"
 import HandleAnime from "./anime"
 export const Router = async(req: Request): Promise<Response> => {
     const url = new URL(req.url)
     const path = url.pathname
     if (path === "/") return new Response("Welcome to bonsumet api! 🎉 \n")
-
     if (/\/anime.*/.test(path)) return HandleAnime(url)
-
-
-
-
     return JsonResponse({message: "Route not found"}, 404)
 }
